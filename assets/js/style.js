48bd6539781017fbf624ae1423b7d02ce0f7368e
Jonathan Marcel <jmaliaga1@gmail.com>
2022-12-06 22:36:28 -0300

    Cambio en JS

--- assets/js/style.js
+++ assets/js/style.js
@@ -6,7 +6,10 @@ $(function () {
 $(document).scroll(function () {
     const y = $("html").scrollTop();
 
+
+
     y > 300 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black")
 
 
+    
 })
